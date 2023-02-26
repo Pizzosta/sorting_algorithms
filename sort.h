b8ae9b836e9482eca3c838fab7517d2379353b8b
Pizzosta <pizzostor@gmail.com>
2023-02-26 11:43:57 -0800

    a function that sorts an array of integers in ascending order using the Bitonic sort algorithm

--- sort.h
+++ sort.h
@@ -27,22 +27,33 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
+
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
+
 void cocktail_sort_list(listint_t **list);
 void swap_list(listint_t **ptr1, listint_t **ptr2, int n);
 void increase_sort(listint_t **ptr, listint_t **limit, listint_t **list);
 void decrease_sort(listint_t **ptr, listint_t **limit, listint_t **list);
+
 void counting_sort(int *array, size_t size);
+
 void merge_sort(int *array, size_t size);
 void msort(int *array, int low, int high, int *buffer);
 void merge(int *a, int low, int middle, int high, int *buff);
 void print_data(char *msg, int *a, int from, int to);
+
 void heap_sort(int *array, size_t size);
 void recursion_heap(int *arr, int i, size_t size, int limit);
 void swap_nums(int *arr, int a, int b);
 void radix_sort(int *array, size_t size);
 
+void bitonic_sort(int *array, size_t size);
+void recursion(int *arr, int low, int high, int bool, size_t size);
+void sort_down(int *arr, int low, int high);
+void sort_up(int *arr, int low, int high);
+void print_bitonic(int *arr, int i, int limit);
 #endif /* _SORT_H_ */
